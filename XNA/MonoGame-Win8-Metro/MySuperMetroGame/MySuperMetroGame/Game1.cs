aa5d5d38593b75aabee7888dc742a27fb3450532
Yannick Comte <comte.yannick@gmail.com>
2012-06-23 13:08:49 +0200

    Ajout du projet Windows 8 Metro pour le tutoriel MonoGame Windows8 et Metro

---
+++ XNA/MonoGame-Win8-Metro/MySuperMetroGame/MySuperMetroGame/Game1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace MySuperMetroGame
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D sprite;
+        Vector2 spritePosition;
+        SpriteFont spriteFont;
+        
+        public Game1() : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            spritePosition = new Vector2(400, 250);
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            spriteFont = Content.Load<SpriteFont>("spriteFont");
+            sprite = Content.Load<Texture2D>("player-status");
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(sprite, spritePosition, Color.White);
+            spriteBatch.DrawString(spriteFont, "Hello MonoGame ! ", new Vector2(100, 25), Color.SeaShell, 0.0f, Vector2.Zero, new Vector2(1.5f, 1.5f), SpriteEffects.None, 1.0f);
+            spriteBatch.End();
+            
+            base.Draw(gameTime);
+        }
+    }
+}
